548944e239e3130d580e2dd3aedf61e13bf8ccff
Derul0 <derulozsy@gmail.com>
2026-02-24 18:20:47 +0800

    feat: remove unnecssary parts

--- tests/test_gpu_pipeline_import.py
+++
@@ -1,7 +0,0 @@
-import importlib
-
-
-def test_gpu_pipeline_module_is_importable():
-    module = importlib.import_module("processors.gpu_pipeline")
-    assert hasattr(module, "run_segmentation_pipeline_gpu")
-
